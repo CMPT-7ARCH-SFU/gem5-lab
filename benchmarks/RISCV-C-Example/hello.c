d50cc808da2b6fd70956209e618d5321e5552ede
Arrvindh Shriraman <ashriram@cs.sfu.ca>
2021-03-01 13:37:38 -0800

    Added C example

---
+++ benchmarks/RISCV-C-Example/hello.c
@@ -0,0 +1,11 @@
+#include <m5ops.h>
+
+
+
+
+void main(){
+  // IDs are not used right now for RISCV 
+  m5_work_begin(0, 0);
+  printf("Hello World");
+  m5_work_end(0, 0);
+}
--- benchmarks/array.riscv.s
+++ benchmarks/array.riscv.s
@@ -9,13 +9,23 @@ asm_main_after_prologue:
     li a7,64
     ecall
 
+    li t0, 5
+    la t1, src
+    la t2, dst
+loop:
+    lw t4,0(t1)
+    sw t4,0(t2)
+    addi t1,t1,4
+    addi t2,t2,4
+    addi t0,t0,-1
+    bnez t0, loop
+
     li a0,1     /* stdout */
     la a1,msg2   /* buffer */
     li a2,15  /* len */
     li a7,64
     ecall
 
-    la t3,src    
     /* exit */
     li a0, 0     /* exit status */
     li a7, 93    /* syscall number */
@@ -23,5 +33,14 @@ asm_main_after_prologue:
 .data
 msg1:	.ascii "---ROIBegin---\n"
 msg2:   .ascii "---ROIEnd---\n\n\n"
+character: .ascii "Copy  \n" 
 src:    .word 0x1 
-dst:    .word 0x0
+        .word 0x2 
+        .word 0x3 
+        .word 0x4 
+        .word 0x5
+dst:    .word 0x0 
+        .word 0x0
+        .word 0x0
+        .word 0x0
+        .word 0x0
