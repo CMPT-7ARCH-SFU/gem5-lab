63a087a277f2c3156a4fe23100cafe5d403e2f98
ygk1 <ygk1@cs-arch-srv03.cmpt.sfu.ca>
2022-09-20 21:50:53 -0700

    restored compile.sh

--- benchmarks/compile.sh
+++ benchmarks/compile.sh
@@ -1,7 +1,4 @@
 $RISCV_HOME/bin/riscv64-unknown-elf-as -g -o main.o main.S
 $RISCV_HOME/bin/riscv64-unknown-elf-as -g -o quicksort.o quicksort.S
 $RISCV_HOME/bin/riscv64-unknown-elf-ld -o quicksort main.o quicksort.o
-
-echo $PATH
-
 rm -r *.o
